bd8fb38fbda834476a7f0c3523ae78880941312c
doringraf <35940476+doringraf@users.noreply.github.com>
2020-02-29 15:50:13 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,8 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Reservation Canceling");
+        
+                System.out.println("Goodbye Git"); // this line is from github by the way ;-;
+
     }
 }
