5066babfba5a2c15fc91fb8cd34812cc83d1769f
doringraf <35940476+doringraf@users.noreply.github.com>
2020-02-29 15:56:31 -0500

    Update LocatingMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -6,5 +6,10 @@ public class LocatingMap {
 
     public static void main(String[] args) {
         System.out.println("Testing a new update");
+
+    
+    
+            System.out.println("GITHUB change !!! is made !!! and might cause a problem ");
+
     }
 }
