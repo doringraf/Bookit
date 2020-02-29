1546157a5464e63b97f21a95ce8cf168255de09e
doringraf <doringraf@gmail.com>
2020-02-29 15:57:11 -0500

    new

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -1,10 +1,10 @@
 package test;
 
 public class LocatingMap {
-
-    /////
-
     public static void main(String[] args) {
         System.out.println("Testing a new update");
+
+
+        System.out.println("Some change has been made");
     }
 }
